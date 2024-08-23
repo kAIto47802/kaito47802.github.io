435aa75eb5100d3e54037a2f5b3044ee25801ba9
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2024-08-24 06:14:19 +0900

    Update lint and format settings

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 2,
+  trailingComma: 'all',
+  jsxSingleQuote: true,
+};
