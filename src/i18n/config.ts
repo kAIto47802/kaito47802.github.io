2c9989fe3a9734b8a4aa6f2cfceebc6376364f90
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2024-08-24 04:50:31 +0900

    Initial commit

---
+++ src/i18n/config.ts
@@ -0,0 +1,29 @@
+import i18n, { use } from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import translationsEn from './locales/en.json';
+import translationsJa from './locales/ja.json';
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      en: {
+        translation: translationsEn,
+      },
+      ja: {
+        translation: translationsJa,
+      },
+    },
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['cookie'],
+      lookupCookie: 'lang',
+    },
+  });
+
+export default i18n;
