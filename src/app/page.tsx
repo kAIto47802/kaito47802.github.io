add3a13cccfd29e51d1bdef1c2e6aad7bddefbf3
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2024-08-26 05:06:47 +0900

    Update basic profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
 'use client';
+import { Noticia_Text } from 'next/font/google';
+import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
 import styles from './page.module.css';
 
+import nextConfig from '../../next.config.mjs';
+const BASE_PATH = nextConfig.basePath || '';
+
+const noticia = Noticia_Text({ weight: '400', style: 'italic', subsets: ['latin'] });
+
 const Home = () => {
   const { t } = useTranslation();
-  return <main className={styles.main}>{t('aaa')}</main>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.basic}>
+        <Image
+          src={`${BASE_PATH}/icon.jpg`}
+          alt='icon'
+          width={200}
+          height={200}
+          style={{ borderRadius: '50%' }}
+        />
+        <div className={styles.basicText}>
+          <div className={`${styles.name} ${noticia.className}`}>kAIto47802</div>
+          <div className={styles.affiliation}>{t('basic.affiliation')}</div>
+        </div>
+      </div>
+    </main>
+  );
 };
 export default Home;
