6fb1a2a677c823d2c9ec5ae27064d562bed7a2f3
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2024-09-09 21:59:48 +0900

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,11 @@ export const metadata: Metadata = {
   title: "kAIto47802's Profile",
   description: "This is kAIto47802's profile page.",
   metadataBase: new URL('https://kaito47802.github.io'),
-  openGraph: { images: 'https://kaito47802.github.io/opengraph-image.png' },
-  twitter: { images: 'https://kaito47802.github.io/twitter-image.png' },
+  openGraph: { images: 'opengraph-image.png' },
+  twitter: {
+    card: 'summary_large_image',
+    images: 'twitter-image.png',
+  },
 };
 
 const RootLayout = ({
