e9649be4431bfa8a793ac42cc049ce94dacf0176
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-04-06 01:45:55 +0200

    Update color

--- src/components/pure/GitHubIcon.tsx
+++ src/components/pure/GitHubIcon.tsx
@@ -18,7 +18,7 @@ const GitHubIcon = ({
       <Image
         src={
           repo === 'kAIto47802'
-            ? 'https://img.shields.io/badge/dynamic/json.svg?label=GitHub&logo=github&style=flat&url=https://kaito47802.github.io/data/github-stars.json&query=$.totalStars&prefix=Stars%20&labelColor=181717&color=1c7ed6'
+            ? 'https://img.shields.io/badge/dynamic/json.svg?label=GitHub&logo=github&style=flat&url=https://kaito47802.github.io/data/github-stars.json&query=$.totalStars&prefix=Stars%20&labelColor=181717'
             : withStar
               ? `https://img.shields.io/badge/dynamic/json.svg?label=GitHub&logo=github&style=flat&url=https://api.github.com/repos/${repo}&query=$.stargazers_count&prefix=Stars%20&labelColor=181717`
               : 'https://img.shields.io/badge/-GitHub-181717.svg?logo=github&style=flat'
